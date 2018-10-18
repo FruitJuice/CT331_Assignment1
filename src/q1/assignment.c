af345114f1ec4b3a0d5ac3c65f2dec9465700492
sndri <sndriuss@gmail.com>
2018-10-18 14:56:52 +0100

    Finished Q1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+  int a;
+  int* b;
+  long c;
+  double* d;
+  char** e;
+  printf("Int size: %d \n",sizeof(a));
+  printf("Int pointer size: %d\n",sizeof(b));
+  printf("Long size: %d\n",sizeof(c));
+  printf("Double pointer size: %d\n",sizeof(d));
+  printf("Int pointer size: %d\n",sizeof(e));
+
   return 0;
 }
